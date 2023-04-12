52478decdaf43be3abed622f1c9ac35d8f3c4017
Lucas Dias <lucas.dortiz17@gmail.com>
2023-04-12 13:38:17 -0300

    Aula 04 - Funcoes

---
+++ 04 - Funcoes/aula.js
@@ -0,0 +1,30 @@
+/* 01 - Exemplo (Imprime o nome da pessoa)
+function sayMyName(name) {
+    console.log('Your name is ' + name);
+}
+sayMyName('Lucas'); 
+*/
+
+/* 02 - Exemplo (Retorna o quadrado de 10)
+function quadrado(valor) {
+    return valor * valor;
+}
+
+const quadradoDeDez = quadrado(10);
+console.log(quadradoDeDez);
+*/
+
+/* 03 - Exemplo (Retorna o valor com Juros)
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrescimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrescimo;
+}
+
+console.log(incrementarJuros(100, 10));
+*/
+// Executar tudo dentro da main e chamar apenas ela.
+function main() {
+    console.log('Programa principal');
+}
+
+main();
---
+++ 04 - Funcoes/imc-com-funcoes.js
@@ -0,0 +1,44 @@
+/*
+  2) O IMC - Indice de Massa Corporal é um critério da Organização Mundial de Saúde para dar uma indicação sobre
+     a condição de peso de uma pessoa adulta.
+    
+    Formula do IMC:
+    IMC = peso / (altura * altura)
+
+    Elabore um algoritimo que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo.
+
+    IMC em adultos Condição:
+    - Abaixo de 18.5 Abaixo do peso;
+    - Entre 18.5 e 25 Peso normal;
+    - Entre 25 e 30 Acima do peso;
+    - Entre 30 e 40 Obeso;
+    - Acima de 40 Obesidade grave;
+*/
+
+
+function calcularIMC(peso, altura) {
+    return peso / Math.pow(altura, 2);
+}
+
+function classificarIMC(imc) {
+    if (imc < 18.5) {
+        return 'Abaixo do peso';
+      } else if (imc >= 18.5 && imc < 25) {
+        return 'Peso normal';
+      } else if (imc >= 25 && imc < 30) {
+        return  'Acima do peso';
+      } else if (imc >= 30 && imc < 40) {
+        return 'Obeso';
+      } else {
+        return 'Obesidade grave';
+      }
+}
+
+function main() {
+    const peso = 90;
+    const altura = 1.72;
+    const imc = calcularIMC(peso, altura);
+    console.log(imc);
+    console.log(classificarIMC(imc));
+}
+main();
